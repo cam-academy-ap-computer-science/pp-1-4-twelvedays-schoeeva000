0f36a3902385ba9696c8928d3aa113eeb1384ce7
wooki <wooki@HowlingPirates>
2019-09-16 21:38:44 -0700

    Name: Evan Schoening Date: 9/16/19 Days of Christmas extra credit

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -1,9 +1,153 @@
 
 public class TwelveDays {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { //wow this is a long main
+		System.out.print("On the first ");
+		sent();
+		partridge();
+		System.out.print("On the second ");
+		sent();
+		turtle();
+		partridge();
+		System.out.print("On the third ");
+		sent();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the fourth ");
+		sent();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the fifth ");
+		sent();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the sixth ");
+		sent();
+		geese();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the seventh ");
+		sent();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the eighth ");
+		sent();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the ninth ");
+		sent();
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the tenth ");
+		sent();
+		lords();
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the eleventh");
+		sent();
+		pipers();
+		lords();
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
+		System.out.print("On the twelfth ");
+		sent();
+		drummers();
+		pipers();
+		lords();
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		turtle();
+		partridge();
 
+	}
+	
+	//wow this is a lot of static methods
+	
+	public static void sent() {
+		System.out.println("day of Christmas,\nMy true love sent to me");
+	}
+	
+	public static void partridge() {
+		System.out.println("a partridge in a pear tree.\n");
+	}
+	public static void turtle() {
+		System.out.println("two turtle doves, and");
+	}
+	public static void hens() {
+		System.out.println("three French hens,");
+	}
+	public static void birds() {
+		System.out.println("four calling birds,");
+	}
+	public static void rings() {
+		System.out.println("five golden rings,");
+	}
+	public static void geese() {
+		System.out.println("six geese a-laying,");
+	}
+	public static void swans() {
+		System.out.println("seven swans a-swimming,");
+	}
+	public static void maids() {
+		System.out.println("eight maids a-milking,");
+	}
+	public static void ladies() {
+		System.out.println("nine ladies dancing,");
+	}
+	public static void lords() {
+		System.out.println("ten lords a-leaping,");
+	}
+	public static void pipers() {
+		System.out.println("eleven pipers piping,");
+	}
+	public static void drummers() {
+		System.out.println("Twelve drummers drumming,");
 	}
 
 }
