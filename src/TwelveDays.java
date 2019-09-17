b0c62cd79bc8d4bd0ffd169fa5ad8d3025b8691b
schoeeva000 <schoeeva000@CAM-LAB-26.bgsd.k12.wa.us>
2019-09-17 08:01:58 -0700

    allDone

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -1,127 +1,60 @@
 
 public class TwelveDays {
 
-	public static void main(String[] args) { //wow this is a long main
-		ot();
+	public static void main(String[] args) {
+		onThe();
 		System.out.print("first ");
 		sent();
 		partridge();
-		ot();
+		onThe();
 		System.out.print("second ");
 		sent();
 		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("third ");
 		sent();
 		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("fourth ");
 		sent();
 		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("fifth ");
 		sent();
 		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("sixth ");
 		sent();
 		geese();
-		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("seventh ");
 		sent();
 		swans();
-		geese();
-		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("eighth ");
 		sent();
 		maids();
-		swans();
-		geese();
-		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("ninth ");
 		sent();
 		ladies();
-		maids();
-		swans();
-		geese();
-		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("tenth ");
 		sent();
 		lords();
-		ladies();
-		maids();
-		swans();
-		geese();
-		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("eleventh");
 		sent();
 		pipers();
-		lords();
-		ladies();
-		maids();
-		swans();
-		geese();
-		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-		ot();
+		onThe();
 		System.out.print("twelfth ");
 		sent();
 		drummers();
-		pipers();
-		lords();
-		ladies();
-		maids();
-		swans();
-		geese();
-		rings();
-		birds();
-		hens();
-		turtle();
-		partridge();
-
 	}
 	
 	//wow this is a lot of static methods
 	
-	public static void ot() {
+	public static void onThe() {
 		System.out.print("On the ");  //prints "On the " in order to reduce redundancy
 	}
 	public static void sent() {
@@ -133,36 +66,47 @@ public class TwelveDays {
 	}
 	public static void turtle() {
 		System.out.println("two turtle doves, and");
+		partridge();
 	}
 	public static void hens() {
 		System.out.println("three French hens,");
+		turtle();
 	}
 	public static void birds() {
 		System.out.println("four calling birds,");
+		hens();
 	}
 	public static void rings() {
 		System.out.println("five golden rings,");
+		birds();
 	}
 	public static void geese() {
 		System.out.println("six geese a-laying,");
+		rings();
 	}
 	public static void swans() {
 		System.out.println("seven swans a-swimming,");
+		geese();
 	}
 	public static void maids() {
 		System.out.println("eight maids a-milking,");
+		swans();
 	}
 	public static void ladies() {
 		System.out.println("nine ladies dancing,");
+		maids();
 	}
 	public static void lords() {
 		System.out.println("ten lords a-leaping,");
+		ladies();
 	}
 	public static void pipers() {
 		System.out.println("eleven pipers piping,");
+		lords();
 	}
 	public static void drummers() {
 		System.out.println("Twelve drummers drumming,");
+		pipers();
 	}
 
 }
